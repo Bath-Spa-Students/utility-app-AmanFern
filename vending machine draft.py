c5762b0a991d53fee800b7c9d083ee48bdac6e6a
AmanFern <amanming300@gmail.com>
2023-12-19 11:14:47 +0400

    vending machine draft

---
+++ vending machine draft.py
@@ -0,0 +1,74 @@
+import time
+import sys
+
+def typewrite(str):
+    for char in str:
+        time.sleep(0.08)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+typewrite("\nHello there!\n")
+time.sleep(0.5)
+typewrite("Welcome to Aman's Vending Machine\n")
+
+vending_inventory = {
+    "Food": {
+        "A1": {"Name": "Twix", "Cost": 3.00, "Stock": 15},
+        "A2": {"Name": "Chocolate Wafers", "Cost": 5.00, "Stock": 8},
+        "A3": {"Name": "Strawberry Wafers", "Cost": 5.00, "Stock": 8},
+        "A4": {"Name": "Hello Panda", "Cost": 3.50, "Stock": 15}
+    },
+
+    "Beverages": {
+        "B1": {"Name": "Mango Juice", "Cost": 2.00, "Stock": 15},
+        "B2": {"Name": "Strawberry Milk", "Cost": 2.50, "Stock": 12},
+        "B3": {"Name": "Chocolate Milk", "Cost": 2.50, "Stock": 12},
+        "B4": {"Name": "Iced Tea", "Cost": 4.00, "Stock": 10}
+    }
+}
+
+print("\n")
+typewrite("SELECT YOUR FOOD AND BEVERAGES\n")
+print("\n")
+print("                               * Food *")
+
+food_options = vending_inventory["Food"]
+
+for code, food in food_options.items():
+    typewrite(f"{code}. {food['Name']} - AED{food['Cost']} \n")
+print()
+print("                               * Beverages *")
+beverage_options = vending_inventory["Beverages"]
+for code, drink in beverage_options.items():
+    typewrite(f"{code}. {drink['Name']} - AED{drink['Cost']} \n")
+print()
+
+while True:
+    selected_code = input("Enter the code of the item you want to purchase: ")
+
+    if selected_code in food_options:
+        selected_item = food_options[selected_code]
+    elif selected_code in beverage_options:
+        selected_item = beverage_options[selected_code]
+    else:
+        print("Invalid code. Please try again.")
+        continue
+
+    if selected_item['Stock'] <= 0:
+        print("Out of stock. Please choose another item.")
+        continue
+
+    cash_inserted = float(input("Enter the amount of money you are inserting: "))
+
+    if cash_inserted < selected_item['Cost']:
+        print("Not enough money. Please try again.")
+        continue
+
+    change = cash_inserted - selected_item['Cost']
+    selected_item['Stock'] -= 1  # Deduct one from stock
+    typewrite(f"Dispensing {selected_item['Name']}. Your change is AED{change:.2f} \n")
+
+    if input("Do you want to buy another item? (yes/no): ").lower() != 'yes':
+        break
+
+typewrite("Thank you! Enjoy your food :)\n")
